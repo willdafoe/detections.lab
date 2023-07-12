a118371e676bcaf449c8d19aa2f4e904466ae243
ralph-brynard <ralph@thebrynards.com>
2023-07-12 10:17:16 -0500

    local.existing_resource `try` logic needs refining

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "azurerm_resources" "existing" {
-  count               = local.use_existing ? 1 : 0
+  count               = local.use_existing ? 0 : 1 # Changing to false to prevent data source from running
   name                = local.existing_resource.name
   resource_group_name = local.existing_resource.resource_group_name
 }
