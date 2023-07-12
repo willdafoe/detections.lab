9707fc97012714e04febd9c7f388893ebbaff9ff
ralph-brynard <ralph@thebrynards.com>
2023-07-12 10:23:05 -0500

    removing type and required_tags values from local.existing_resource

--- locals.tf
+++ locals.tf
@@ -12,10 +12,11 @@ locals {
   )]
 
   # Local value attempts to build/evaluate either a resource name or a resource_group name. Based on which of the two is not null. Additionally, a mapping to a resource type is built specifically for the purpose of filtering the returned data from the data sources to limit the scope of the returned data.
+
+  #Build local.name value. Previously used `try()` in local.existing_resource.name, however, this returns back first try value. This is not the desired behavior. Instead, we want to return the value of the variable that is not null. This is accomplished by using the `!()` function. This function returns the value of the variable that is not null. If both variables are null, then the function returns null.
+
   existing_resource = {
-    name                = try(var.resource_group_name, var.vnet_name, null)
+    name                = !(var.resource_group_name == null) ? var.resource_group_name : !(var.vnet_name == null) ? var.vnet_name : null
     resource_group_name = !(var.resource_group_name == null) ? var.resource_group_name : null
-    type                = try(var.resource_group_name != null ? "Microsoft.Resources/resourceGroups" : null, var.vnet_name != null ? "Microsoft.Network/virtualNetworks" : null, null)
-    required_tags       = try(var.filter_tags, null)
   }
 }
